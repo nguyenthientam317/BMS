70b1f8d4554bda1686e6c4e0ee6160ea71e0e038
NguyenNgocPhu1 <45066813+NguyenNgocPhu1@users.noreply.github.com>
2019-04-05 14:59:57 +0700

    Update-Add
    Add: CartModel
    Update menu Layout Client
    5/4/2019

---
+++ Book Management System/Book Management System/Common/CartModel.cs
@@ -0,0 +1,23 @@
+﻿using Book_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book_Management_System.Common
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public double TotalAmount { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+    //Object return Layout
+    public class CartResponseModel
+    {
+        public bool Result { get; set; }
+        public string Message { get; set; }
+        public double Total { get; set; }
+    }
+
+}
